64ef8d8a81a7768e376017ba1e55b7475875947f
Thomas W Nyberg <tomnyberg@gmail.com>
2013-02-18 17:01:37 -0500

    BugFix: Added ignore-case flag.

--- scripts/compute_averages.sh
+++ scripts/compute_averages.sh
@@ -13,5 +13,5 @@ cat $DATA/Case_Data_from_San_Francisco_311.csv \
     | python $SRC/reformat.py \
     | python $SRC/timeopen.py -d \| \
     | python $SRC/cut.py -d \| -l category,status,timeopen \
-    | body sort --field-separator=\|  --key=1,2 \
+    | body sort --ignore-case --field-separator=\|  --key=1,2 \
     | python $SRC/averager.py -d \| --key=category,status --fieldnames=timeopen
