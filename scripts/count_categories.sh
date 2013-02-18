ac0bbb92ff64fef7417eb803e6164619e607070c
Ben Cheng <ben.y.cheng@gmail.com>
2013-02-18 14:50:40 -0500

    finished count_categories script

--- scripts/count_categories.sh
+++ scripts/count_categories.sh
@@ -8,3 +8,13 @@ DATA=../data
 ## Print list of categories and their size
 # Use cat, reformat.py, cut.py, tail, sort, uniq, sort
 #
+
+
+cat $DATA/Case_Data_from_San_Francisco_311.csv \
+    | python $SRC/reformat.py \
+    | python $SRC/cut.py -d \| -l category \
+    | tail -n +2 \
+    | sort -k1 \
+    | uniq -c \
+    | sort -r -n -k1
+
