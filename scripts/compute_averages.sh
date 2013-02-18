d7271ad9ab6919df0b754727dacafa62d78cc6d7
Thomas W Nyberg <tomnyberg@gmail.com>
2013-02-18 13:14:29 -0500

    Bug: Fixed separator in sort.

--- scripts/compute_averages.sh
+++ scripts/compute_averages.sh
@@ -12,5 +12,5 @@ DATA=../data
 cat $DATA/Case_Data_from_San_Francisco_311.csv \
     | python $SRC/reformat.py \
     | python $SRC/timeopen.py -d \| \
-    | body sort -k4 -k8 \
+    | body sort --field-separator=\| -k4 -k8 \
     | python $SRC/averager.py -d \| -k status,category -f timeopen
